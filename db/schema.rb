196226f0048a71e1c90fcf7ba4bba763b21e2d83
Rich Ogrady <rjogrady21@gmail.com>
2018-05-04 21:04:40 +0200

    models migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_04_180055) do
+ActiveRecord::Schema.define(version: 2018_05_04_190352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "poems", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "poet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["poet_id"], name: "index_poems_on_poet_id"
+  end
+
+  create_table "poets", force: :cascade do |t|
+    t.string "name"
+    t.date "date_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +48,5 @@ ActiveRecord::Schema.define(version: 2018_05_04_180055) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "poems", "poets"
 end
