2ee2c4be1b5ad67329952418190e27609d237b17
Rich Ogrady <rjogrady21@gmail.com>
2018-05-12 15:41:08 +0200

    Db rollbacked

--- db/migrate/20180512083851_create_categories_and_poems.rb
+++
@@ -1,8 +0,0 @@
-class CreateCategoriesAndPoems < ActiveRecord::Migration[5.2]
-  def change
-    create_table :categories_poems, id: false do |t|
-      t.belongs_to :category, index: true
-      t.belongs_to :poem, index: true
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_12_083851) do
+ActiveRecord::Schema.define(version: 2018_05_12_083800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 2018_05_12_083851) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "categories_poems", id: false, force: :cascade do |t|
-    t.bigint "category_id"
-    t.bigint "poem_id"
-    t.index ["category_id"], name: "index_categories_poems_on_category_id"
-    t.index ["poem_id"], name: "index_categories_poems_on_poem_id"
   end
 
   create_table "poems", force: :cascade do |t|
