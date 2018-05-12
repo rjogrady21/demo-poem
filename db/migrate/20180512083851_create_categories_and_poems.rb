28ad097be0378492cc3aa7454a2ec7316dfa3ee2
Rich Ogrady <rjogrady21@gmail.com>
2018-05-12 10:51:06 +0200

    Categories model, controller and AR associations set up

---
+++ db/migrate/20180512083851_create_categories_and_poems.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableCategoryPoem < ActiveRecord::Migration[5.2]
+  def change
+    create_join_table :categories_poems, id: false do |t|
+      t.belongs_to :category, index: true
+      t.belongs_to :poem, index: true
+    end
+  end
+end
